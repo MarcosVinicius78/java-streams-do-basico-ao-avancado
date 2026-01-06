c5eecb8d18a096482f7e418c7eff2055bbfef8bd
Your Name <you@example.com>
2026-01-05 23:58:55 -0300

    usando o metodo reduce para somar alguns valores

---
+++ fundamentos/Exercicio3.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        List<Integer> listaNumeros = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        int total = listaNumeros.stream()
+                .reduce(0, (subTotal, item) -> subTotal + item);
+
+        System.out.println(total);
+    }
+}
