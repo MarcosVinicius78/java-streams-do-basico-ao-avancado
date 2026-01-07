8d3c5f14a56c44c35ab08628cfaddd8ad1ee27eb
Your Name <you@example.com>
2026-01-06 21:29:38 -0300

    fix: colocando comentarios no exercicio 3

--- fundamentos/Exercicio3.java
+++ fundamentos/Exercicio3.java
@@ -4,6 +4,8 @@ public class Exercicio3 {
     public static void main(String[] args) {
         List<Integer> listaNumeros = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
+        // Eu poderia usar (0, Integer::sum)
+        // 0 é o valor inicial, depois e o que ele vai fazer
         int total = listaNumeros.stream()
                 .reduce(0, (subTotal, item) -> subTotal + item);
 
